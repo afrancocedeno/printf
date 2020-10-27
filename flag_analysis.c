74c2e418b2cf2158b7193000ba78072e6ba00310
Andrés Aristizábal <69760731+aristizabaru@users.noreply.github.com>
2020-10-26 21:25:42 -0500

    Update flag_analysis.c

--- flag_analysis.c
+++ flag_analysis.c
@@ -20,7 +20,7 @@ char *analyse_code(char *directive_string, formats *formatos,
 			return (formatos[i].get_substring(pa));
 		i++;
 	}
-	return(1);
+	exit(1);
 }
 /**
  * validate_directive - validate code directive
@@ -49,5 +49,5 @@ int validate_directive(formats *formatos, char *directive_string,
 		length++;
 	}
 	/*if not format found*/
-	return(1);
+	exit(1);
 }
